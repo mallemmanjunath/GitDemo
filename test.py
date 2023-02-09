9d59e0c96c64c42d4eb3aaa7f3f57bedc1816b7b
Manjunath <mallemmanjunath@gmail.com>
2023-02-09 16:19:32 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,4 @@
 print("This is test file")
 print("added this from github.com")
-<<<<<<< HEAD
 print("This is adding at 4:10 PM IST")
-=======
-print("This I am adding at 4:13")
->>>>>>> 585cd3de77a061594b62eb71ec0caff4f8e2d9fd
+
